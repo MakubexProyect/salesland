158ea3ead3fe1a0a9427771281feb62741c7762d
Celso Diaz Marquez <celso.diaz@sondaperu.com.pe>
2015-12-07 17:40:29 -0500

    Commit SNDNC412 07/12/2015

---
+++ tarea_programada.ps1
@@ -0,0 +1,6 @@
+﻿$A=New-ScheduledTaskAction -Execute ‘%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe’ -Argument ‘Backup-GPO -All -Path Z:\GPOS_AD’
+$T=New-ScheduledTaskTrigger -AtStartup
+$U=New-ScheduledTaskPrincipal -UserId “$($env:USERDOMAIN)\$($env:USERNAME)” -LogonType ServiceAccount
+$S=New-Scheduledtasksettingsset
+$D=Set-ScheduledTask -Action $A -Principal $U -Trigger $T -Settings $S
+Register-ScheduledTask SALESLAND -InputObject $D
