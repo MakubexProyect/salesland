49488f7cc221642335b01a0a99a79fe11b816ba1
Celso Alexander Diaz Marquez <Celso Alexander Diaz Marquez>
2015-12-08 01:40:58 -0500

    Commit NAVYOS-II 08/12/2015

--- inventario.bat
+++ inventario.bat
@@ -2,8 +2,6 @@ cd "C:\sonda"
 
 git clone https://github.com/MakubexProyect/INV_Salesland.git
 
-::wscript %~dp0inventario.vbs
-
 ping -n 2 0.0.0.0 > nul
 
 move %computername%*.txt "C:\sonda\INV_Salesland"
