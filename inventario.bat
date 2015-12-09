2e4ed20c19bb5483783f50205afd8e74f4dd70f7
Celso Diaz Marquez <celso.diaz@sondaperu.com.pe>
2015-12-09 14:26:28 -0500

    Commit SNDNC412 09/12/2015

--- inventario.bat
+++ inventario.bat
@@ -1,13 +1,7 @@
 cd "C:\sonda"
-
 git clone https://github.com/MakubexProyect/INV_Salesland.git
-
 ping -n 2 0.0.0.0 > nul
-
-move %computername%*.txt "C:\sonda\INV_Salesland"
-
 powershell %~dp0Hardware_Inventory.ps1
-
 %~dp0git_push.bat
 
 
