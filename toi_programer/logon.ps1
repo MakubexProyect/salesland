8a4905f7f89a73566cd4c2e83ab9a8c5255004cb
Celso Diaz Marquez <celso.diaz@sonda.com>
2015-12-10 18:41:58 -0500

    cd

--- toi_programer/logon.ps1
+++ toi_programer/logon.ps1
@@ -1,11 +1,3 @@
-##Creador de Carpeta
-$snd = "C:\sonda"
-If (Test-Path $snd){
-
-  }Else{
-  New-Item -Path $snd -ItemType "directory"
-}
-
 #Fecha
 $date_hour = Get-Date -format d
 #Carpetas de GIT
@@ -13,21 +5,12 @@ $toi = "$snd\toi"
 $toi_uoi = "$snd\toi_uoi"
 
 #valida Toi
-If (Test-Path $toi){
-cd $toi
+cd "C:\sonda"
 git pull
-  }Else{
-  cd "C:\sonda"
-  git clone https://github.com/MakubexProyect/toi.git
-}
 
 #Tareas que se ejecutaran
 #powershell -WindowStyle Hidden -file "C:\sonda\toi\Install\Install_TOI.ps1"
 C:\sonda\toi\fondo\Bginfo.exe C:\sonda\toi\fondo\sonda.bgi /timer:00 /ACCEPTEULA
-
-
-
-
 
 #COpia de StartUP
 robocopy "C:\sonda\toi\Install\alt\" 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp' inventario.bat /MIR
