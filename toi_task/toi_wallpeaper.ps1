19353a02370ba0e6c2389c7b184df6333215c75b
MakubexProyect <celso.diaz@sonda.com>
2015-12-12 15:07:16 -0500

    beta Test

---
+++ toi_task/toi_wallpeaper.ps1
@@ -0,0 +1,7 @@
+﻿##subir archivo
+C:\sonda\toi\fondo\Bginfo.exe C:\sonda\toi\fondo\toi_bginfo.bgi /timer:00 /ACCEPTEULA
+cd "C:\sonda\toi"
+git config --global user.email "celso.diaz@sonda.com"
+git config --global credential.helper wincred
+git pull
+C:\sonda\toi\fondo\Bginfo.exe C:\sonda\toi\fondo\toi_bginfo.bgi /timer:00 /ACCEPTEULA
