787083f36f008174281a134d5b7b628b90498b2c
MakubexProyect <celso.diaz@sonda.com>
2015-12-11 23:42:49 -0500

    NavyOs

--- toi_task/toi_task_update.ps1
+++ toi_task/toi_task_update.ps1
@@ -1,8 +1,21 @@
+#Detener Tareas
+schtasks /End /TN “TOI\TOI-Update”
+schtasks /End /TN “TOI\TOI-Inventario”
+schtasks /End /TN “TOI\TOI-Password"
+
 #IMPORTADO DE TAREA POR XML
 schtasks /create /TN “TOI\TOI-Update” /RU "SYSTEM" /XML "C:\sonda\toi\toi_programer\TOI-Update.xml" /F
-schtasks /create /TN “TOI\TOI-Task” /RU "SYSTEM" /XML "C:\sonda\toi\toi_programer\TOI-Task.xml" /F
+schtasks /create /TN “TOI\TOI-Inventario” /RU "SYSTEM" /XML "C:\sonda\toi\toi_programer\TOI-Inventario.xml" /F
+schtasks /create /TN “TOI\TOI-Password" /RU "SYSTEM" /XML "C:\sonda\toi\toi_programer\TOI-Password.xml" /F
+
+#Start Tareas
+schtasks /Run /TN “TOI\TOI-Update”
+schtasks /Run /TN “TOI\TOI-Inventario”
+schtasks /Run /TN “TOI\TOI-Password"
 
 #Copia de StartUP - CONTINGENCIA
-robocopy "C:\sonda\toi\Install\alt\" 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp' inventario.bat /MIR
-robocopy "C:\sonda\toi\fondo\" 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp' *.lnk
+#robocopy "C:\sonda\toi\Install\alt\" 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp' inventario.bat /MIR
+#robocopy "C:\sonda\toi\fondo\" 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp' *.lnk
 #powershell -WindowStyle Hidden -file "C:\sonda\toi\toi_task\inventario_v2.ps1"
+
+
