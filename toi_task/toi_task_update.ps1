3fbbf3df31ba6fa094cdbe16a666359aeede0645
MakubexProyect <celso.diaz@sonda.com>
2015-12-12 12:28:49 -0500

    version beta 5

--- toi_task/toi_task_update.ps1
+++ toi_task/toi_task_update.ps1
@@ -1,21 +1,20 @@
 #Detener Tareas
-schtasks /End /TN “TOI\TOI-Update”
-schtasks /End /TN “TOI\TOI-Inventario”
-schtasks /End /TN “TOI\TOI-Password"
+#schtasks /End /TN “TOI\TOI-Update”
+#schtasks /End /TN “TOI\TOI-Wallpeaper”
+#schtasks /End /TN “TOI\TOI-System"
 
 #IMPORTADO DE TAREA POR XML
 schtasks /create /TN “TOI\TOI-Update” /RU "SYSTEM" /XML "C:\sonda\toi\toi_programer\TOI-Update.xml" /F
-schtasks /create /TN “TOI\TOI-Inventario” /RU "SYSTEM" /XML "C:\sonda\toi\toi_programer\TOI-Inventario.xml" /F
-schtasks /create /TN “TOI\TOI-Password" /RU "SYSTEM" /XML "C:\sonda\toi\toi_programer\TOI-Password.xml" /F
+schtasks /create /TN “TOI\TOI-Wallpeaper” /RU "SYSTEM" /XML "C:\sonda\toi\toi_programer\TOI-Wallpeaper.xml" /F
+schtasks /create /TN “TOI\TOI-System" /RU "SYSTEM" /XML "C:\sonda\toi\toi_programer\TOI-System.xml" /F
 
 #Start Tareas
-schtasks /Run /TN “TOI\TOI-Update”
-schtasks /Run /TN “TOI\TOI-Inventario”
-schtasks /Run /TN “TOI\TOI-Password"
+#schtasks /Run /TN “TOI\TOI-Update”
+#schtasks /Run /TN “TOI\TOI-Inventario”
+#schtasks /Run /TN “TOI\TOI-Password"
 
 #Copia de StartUP - CONTINGENCIA
 #robocopy "C:\sonda\toi\Install\alt\" 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp' inventario.bat /MIR
-#robocopy "C:\sonda\toi\fondo\" 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp' *.lnk
+robocopy "C:\sonda\toi\fondo\" 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp' *.lnk
 #powershell -WindowStyle Hidden -file "C:\sonda\toi\toi_task\inventario_v2.ps1"
-
 
