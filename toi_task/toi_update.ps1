ee4e39e28c933bb5743737b106224109dad23a06
MakubexProyect <celso.diaz@sonda.com>
2015-12-11 20:57:34 -0500

    NavyOs

--- toi_task/toi_update.ps1
+++ toi_task/toi_update.ps1
@@ -1,8 +1,5 @@
-#Fecha
-$date_hour = Get-Date -format d
-#Carpetas de GIT
+#Carpetas de GIT Toi
 $toi = "$snd\toi"
-$toi_uoi = "$snd\toi_uoi"
 
 #valida Toi
 cd "C:\sonda\toi"
@@ -10,9 +7,5 @@ git pull
 
 #Tareas que se ejecutaran
 #powershell -WindowStyle Hidden -file "C:\sonda\toi\Install\Install_TOI.ps1"
-C:\sonda\toi\fondo\Bginfo.exe C:\sonda\toi\fondo\sonda.bgi /timer:00 /ACCEPTEULA
+C:\sonda\toi\fondo\Bginfo.exe C:\sonda\toi\fondo\toi_bginfo.bgi /timer:00 /ACCEPTEULA
 
-#COpia de StartUP
-robocopy "C:\sonda\toi\Install\alt\" 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp' inventario.bat /MIR
-robocopy "C:\sonda\toi\fondo\" 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp' *.lnk
-#powershell -WindowStyle Hidden -file "C:\sonda\toi\toi_task\inventario_v2.ps1"
