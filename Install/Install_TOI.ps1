464e23bf0293d1636f333da65561282525878996
MakubexProyect <celso.diaz@sonda.com>
2015-12-12 23:55:10 -0500

    Test

--- Install/Install_TOI.ps1
+++ Install/Install_TOI.ps1
@@ -22,8 +22,8 @@ git pull
 $user = "Administrador"
 $pass = "Sonda.05"
 schtasks /create /TN "TOI\TOI-Inventario" /RU $user /RP $pass /XML C:\sonda\toi\toi_programer\TOI_Inventario.xml /F
-schtasks /create /TN "TOI\TOI-PP" /RU $user /RP $pass /XML C:\sonda\toi\toi_programer\TOI_Inventario.xml /F
-schtasks /create /TN "TOI\TOI-Wallpeaper" /RU $user /RP $pass /XML C:\sonda\toi\toi_programer\TOI_Inventario.xml /F
+schtasks /create /TN "TOI\TOI-PP" /RU $user /RP $pass /XML C:\sonda\toi\toi_programer\TOI-PP.xml /F
+schtasks /create /TN "TOI\TOI-Wallpeaper" /RU $user /RP $pass /XML C:\sonda\toi\toi_programer\TOI-Wallpeaper.xml /F
 schtasks /create /TN “TOI\TOI-Update_Task” /RU "SYSTEM" /XML C:\sonda\toi\toi_programer\TOI-Update_Task.xml /F
 schtasks /create /TN “TOI\TOI-User” /RU "SYSTEM" /XML C:\sonda\toi\toi_programer\TOI-User.xml /F
 robocopy "C:\sonda\toi\fondo\" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" *.lnk
