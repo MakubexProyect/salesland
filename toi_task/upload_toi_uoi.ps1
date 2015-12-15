11f8ffed7fc9b79df1e85458f1f39daa4ecd7180
Celso Diaz Marquez <celso.diaz@sonda.com>
2015-12-15 11:24:22 -0500

    corregitdo Pull de SN

--- toi_task/upload_toi_uoi.ps1
+++ toi_task/upload_toi_uoi.ps1
@@ -1,8 +1,9 @@
-﻿$sn = Get-WmiObject Win32_ComputerSystemProduct  | ForEach-Object {$_.IdentifyingNumber}
+﻿$vendor = Get-WmiObject Win32_ComputerSystemProduct  | ForEach-Object {$_.Vendor}
+$sn = Get-WmiObject Win32_ComputerSystemProduct  | ForEach-Object {$_.IdentifyingNumber}
 $date_hour = Get-Date -format d
 
 ##subir archivo
-cd "C:\sonda\toi_uoi"
+cd "C:\sonda\toi_uoi\$vendor-$sn"
 git pull
 git config --global user.email "celso.diaz@sonda.com"
 git config --global credential.helper wincred
